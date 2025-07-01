c92ec1b2b9040f3a18ec95f0ccc672211ce99ffd
BethTawiah <elizabeth.tawiah@amalitech.org>
2025-07-01 19:54:35 +0000

    added navlinks for sidebar(parent) and routed each to its page

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,57 @@
+import { LayoutList, BookMarked, ChartSpline, Sheet, Bell, LogOut} from 'lucide-react';
 import React from 'react';
+import { NavLink } from 'react-router';
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return <div>
+    <ul className="menu font-[var(--font-poppins)] text-white rounded-box w-56 ml-5 ">
+     <li className="menu-title my-7 text-gray-400">EduTrack</li>
+    <li className="mb-7 hover:bg-white hover:text-[var(--color-primary)] rounded-full">
+    <NavLink to="/parent/dashboard">
+      <LayoutList/>
+     
+      Overview
+    </NavLink>
+  </li>
+    <li className="mb-7 hover:bg-white hover:text-[var(--color-primary)] rounded-full">
+    <NavLink to="/parent/subject">
+     <BookMarked/>
+      Subjects
+    </NavLink>
+  </li>
+    <li className="mb-7 hover:bg-white hover:text-[var(--color-primary)] rounded-full">
+    <NavLink to="/parent/attendance">
+      <BookMarked/>
+      Attendance
+    </NavLink>
+  </li>
+    <li className="mb-7 hover:bg-white hover:text-[var(--color-primary)] rounded-full">
+    <NavLink to="/parent/assessment">
+      <ChartSpline/>
+      Assessment
+    </NavLink>
+  </li>
+    <li className="mb-7 hover:bg-white hover:text-[var(--color-primary)] rounded-full">
+    <NavLink to="/parent/term-report">
+      <Sheet/>
+      Term Report 
+    </NavLink>
+  </li>
+    <li className="mb-7 hover:bg-white hover:text-[var(--color-primary)] rounded-full">
+    <NavLink to="/parent/chat">
+      <Bell/>
+      Notification 
+    </NavLink>
+  </li>
+    <li className="my-10 hover:bg-white hover:text-[var(--color-primary)] rounded-full">
+    <NavLink to="/login">
+      <LogOut/>
+      Logout 
+    </NavLink>
+  </li>
+</ul>
+
+</div>;
 };
 
 export default Sidebar;
--- src/layout/ParentLayout.jsx
+++ src/layout/ParentLayout.jsx
@@ -3,13 +3,13 @@ import Sidebar from '../components/Sidebar';
 
 const ParentLayout = () => {
   return (
-    <div className="parent-layout">
-      <div className="main-content">
+    <div className="parent-layout   ">
+      <div className="main-content flex flex-row  ">
         {/* Sidebar for parent navigation */}
         <Sidebar userType="parent" />
 
         {/* This is where the nested route components will render */}
-        <main className="content-area">
+        <main className="content-area bg-white my-20 mx-5 w-[80vw] rounded-2xl">
           <Outlet />
         </main>
       </div>
